4a44c6560d6a85d20a482da579cd12a3231904ea
mehekthaker <mehekthaker@gmail.com>
2018-08-07 17:52:41 +0530

    Add files via upload

---
+++ ResumeProj/src/com/cg/resume/Resume.java
@@ -0,0 +1,44 @@
+package com.cg.resume;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Resume
+ */
+@WebServlet("/Resume")
+public class Resume extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Resume() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		out.println(request.getParameter("firstname"));
+		out.println(request.getParameter("lastname"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
